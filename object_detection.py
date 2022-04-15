8bf3f5ab4e4cb63a58cc77423436fee82fa653ac
Dereje <45657872+firaoll@users.noreply.github.com>
2022-04-15 11:43:44 +0300

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -1,11 +1,10 @@
 import cv2
 
-thres = 0.45 # Threshold to detect object
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3,1280)
 cap.set(4,720)
-cap.set(10,70)
+
 
 classNames= []
 classFile = 'coco.names'
@@ -23,14 +22,14 @@ net.setInputSwapRB(True)
 
 while True:
     success,img = cap.read()
-    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+    classIds, confs, bbox = net.detect(img,confThreshold=0.5)
     print(classIds,bbox)
 
     if len(classIds) != 0:
         for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-            cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+            cv2.rectangle(img,box,color=(0,0,255),thickness=2)
             cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
-                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                        cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
             cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
                         cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 
